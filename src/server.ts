36abd90e086f9fd6cd0de386b18a62b136560f45
zhoushunming <mason.zhou@shopline.com>
2025-04-06 00:24:38 +0800

    feat: test smithery tool config configure

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,159 @@
 
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import {
+  ListPromptsRequestSchema,
+  GetPromptRequestSchema
+} from "@modelcontextprotocol/sdk/types.js";
 import { z } from "zod";
 import packageJson from "../package.json";
 import parser from "./parser";
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-const server = new McpServer({
+const server = new Server({
   name: packageJson.name,
   version: packageJson.version
 }, {
   capabilities: {
-    tools: {}
+    tools: {
+      list: true,
+      call: true
+    }
   }
 });
 
-server.tool('operating_guide', `
-  # Parse Error Stack Trace
+const stacksSchema = z.array(
+  z.object({
+    line: z.number({
+      description: "The line number in the stack trace.",
+    }),
+    column: z.number({
+      description: "The column number in the stack trace.",
+    }),
+    sourceMapUrl: z.string({
+      description: "The URL of the source map file corresponding to the stack trace.",
+    }),
+  })
+);
 
-  This tool allows you to parse error stack traces by mapping them to the corresponding source code locations using source maps.
-  It is particularly useful for debugging production errors where the stack trace points to minified or obfuscated code.
-`, async () => {
+server.setRequestHandler(ListPromptsRequestSchema, async () => {
+  return {
+    prompts: [{
+      name: "operating_guide",
+      description: `
+        # Parse Error Stack Trace
 
-  try {
-    const content = await fs.readFile(path.join(import.meta.dirname, './external/docs/guide.xml'), 'utf-8');
+        This tool allows you to parse error stack traces by mapping them to the corresponding source code locations using source maps.
+        It is particularly useful for debugging production errors where the stack trace points to minified or obfuscated code.
+      `,
+      arguments: []
+    }, {
+      name: "parse_stack",
+      description: `
+        # Parse Error Stack Trace
 
-    return {
-      content: [
+        This tool allows you to parse error stack traces by providing the following:
+        - A downloadable source map URL.
+        - The line and column numbers from the stack trace.
+
+        The tool will map the provided stack trace information to the corresponding source code location using the source map.
+        It also supports fetching additional context lines around the error location for better debugging.
+
+        ## Parameters:
+        - **stacks**: An array of stack trace objects, each containing:
+          - **line**: The line number in the stack trace.
+          - **column**: The column number in the stack trace.
+          - **sourceMapUrl**: The URL of the source map file corresponding to the stack trace.
+
+        - **ctxOffset** (optional): The number of additional context lines to include before and after the error location in the source code. Defaults to 5.
+
+        ## Returns:
+        - A JSON object containing the parsed stack trace information, including the mapped source code location and context lines.
+        - If parsing fails, an error message will be returned for the corresponding stack trace.
+      `,
+      arguments: [
+
         {
-          type: "text",
-          text: content
+          name: "stacks",
+          type: 'array',
+          description: "An array of stack trace objects, each containing line, column, and sourceMapUrl.",
+          items: {
+            type: "object",
+            properties: {
+              line: {
+                type: "number",
+                description: "The line number in the stack trace."
+              },
+              column: {
+                type: "number",
+                description: "The column number in the stack trace."
+              },
+              sourceMapUrl: {
+                type: "string",
+                description: "The URL of the source map file corresponding to the stack trace."
+              }
+            },
+            required: ["line", "column", "sourceMapUrl"]
+          }
+        },
+        {
+          name: "ctxOffset",
+          type: "number",
+          description: "The number of additional context lines to include before and after the error location in the source code.",
+          default: 5
         }
       ]
+    }]
+  };
+});
+
+
+server.setRequestHandler(GetPromptRequestSchema, async (req) => {
+  const { name, arguments: args } = req.params;
+
+  try {
+    switch (name) {
+      case 'operating_guide':
+        const content = await fs.readFile(path.join(import.meta.dirname, './external/docs/guide.xml'), 'utf-8');
+        return {
+          content: [
+            {
+              type: "text",
+              text: content
+            }
+          ]
+        };
+      case 'parse_stack':
+
+        if (!args) {
+          throw new Error("No arguments provided.");
+        }
+
+        const stacks = stacksSchema.parse(args.stacks);
+        const ctxOffset = Number(args.ctxOffset) || 1;
+
+        const parserRes = await parser.batchParseStack(stacks, ctxOffset);
+
+        if (parserRes.length === 0) {
+          throw new Error("No data could be parsed from the provided stack traces.");
+        }
+
+        return {
+          content: [{
+            type: "text", text: JSON.stringify(parserRes.map(e => {
+              if (e.success) {
+                return e;
+              } else {
+                return {
+                  success: false,
+                  msg: e.error.message,
+                }
+              }
+            }))
+          }],
+        };
+      default:
+        throw new Error("Invalid prompt name.");
     }
   } catch (error) {
     return {
@@ -39,74 +161,11 @@ server.tool('operating_guide', `
       content: [
         {
           type: "text",
-          text: "Error reading the guide doc file."
+          text: error instanceof Error ? error.message : error
         }
       ]
     }
   }
-})
-
-server.tool("parse_stack", `
-  # Parse Error Stack Trace
-
-  This tool allows you to parse error stack traces by providing the following:
-  - A downloadable source map URL.
-  - The line and column numbers from the stack trace.
-
-  The tool will map the provided stack trace information to the corresponding source code location using the source map.
-  It also supports fetching additional context lines around the error location for better debugging.
-
-  ## Parameters:
-  - **stacks**: An array of stack trace objects, each containing:
-    - **line**: The line number in the stack trace.
-    - **column**: The column number in the stack trace.
-    - **sourceMapUrl**: The URL of the source map file corresponding to the stack trace.
-
-  - **ctxOffset** (optional): The number of additional context lines to include before and after the error location in the source code. Defaults to 5.
-
-  ## Returns:
-  - A JSON object containing the parsed stack trace information, including the mapped source code location and context lines.
-  - If parsing fails, an error message will be returned for the corresponding stack trace.
-`, {
-  stacks: z.array(
-    z.object({
-      line: z.number({
-        description: "The line number in the stack trace.",
-      }),
-      column: z.number({
-        description: "The column number in the stack trace.",
-      }),
-      sourceMapUrl: z.string({
-        description: "The URL of the source map file corresponding to the stack trace.",
-      }),
-    })
-  ),
-  ctxOffset: z.number({
-    description: "The number of additional context lines to include before and after the error location in the source code.",
-  }).optional().default(5),
-}, async ({ stacks, ctxOffset }) => {
-  const parserRes = await parser.batchParseStack(stacks, ctxOffset);
-
-  if (parserRes.length === 0) {
-    return {
-      isError: true,
-      content: [{ type: "text", text: "No data could be parsed from the provided stack traces." }],
-    }
-  }
-
-  return {
-    content: [{
-      type: "text", text: JSON.stringify(parserRes.map(e => {
-        if (e.success) {
-          return e;
-        } else {
-          return {
-            success: false,
-            msg: e.error.message,
-          }
-        }
-      }))
-    }],
-  }
 });
+
 export default server;
